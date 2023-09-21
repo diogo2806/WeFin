fafb205999dc9db04521b3b63b484e638504409f
diogo2806 <diogo_2806@hotmail.com>
2023-09-20 21:44:28 -0300

    update

--- GestaoEmprestimosEmpresaX/src/main/java/br/com/wefin/GestaoEmprestimosEmpresaX/service/EmprestimoService.java
+++ GestaoEmprestimosEmpresaX/src/main/java/br/com/wefin/GestaoEmprestimosEmpresaX/service/EmprestimoService.java
@@ -1,13 +1,13 @@
 package br.com.wefin.GestaoEmprestimosEmpresaX.service;
 
-import br.com.wefin.GestaoEmprestimosEmpresaX.dto.EmprestimoDTO;
 import br.com.wefin.GestaoEmprestimosEmpresaX.model.Emprestimo;
+import br.com.wefin.GestaoEmprestimosEmpresaX.model.Pessoa;
 import br.com.wefin.GestaoEmprestimosEmpresaX.repository.EmprestimoRepository;
+import br.com.wefin.GestaoEmprestimosEmpresaX.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class EmprestimoService {
@@ -15,57 +15,46 @@ public class EmprestimoService {
     @Autowired
     private EmprestimoRepository emprestimoRepository;
 
-    // Padrão Facade: Este método simplifica a obtenção de todos os empréstimos
-    public List<EmprestimoDTO> findAll() {
-        return emprestimoRepository.findAll().stream()
-                .map(this::convertToDTO)
-                .collect(Collectors.toList());
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    // Método para buscar todos os empréstimos
+    public List<Emprestimo> findAll() {
+        return emprestimoRepository.findAll();
     }
 
-    // Padrão Facade: Este método simplifica a obtenção de um empréstimo por ID
-    public EmprestimoDTO findById(Long id) {
+    // Método para buscar um empréstimo por ID
+    public Emprestimo findById(Long id) {
         return emprestimoRepository.findById(id)
-                .map(this::convertToDTO)
                 .orElseThrow(() -> new EntityNotFoundException("Empréstimo não encontrado"));
     }
 
-    // Princípio SOLID: Single Responsibility
-    // Este método tem a única responsabilidade de salvar um empréstimo
-    public EmprestimoDTO save(EmprestimoDTO emprestimoDTO) {
-        Emprestimo emprestimo = convertToEntity(emprestimoDTO);
-        return convertToDTO(emprestimoRepository.save(emprestimo));
+    // Método para criar um novo empréstimo
+    public Emprestimo create(Emprestimo emprestimo) {
+        // Validações de negócio (identificador válido, limites de empréstimo, etc.)
+        Pessoa pessoa = pessoaRepository.findById(emprestimo.getPessoaId())
+                .orElseThrow(() -> new EntityNotFoundException("Pessoa não encontrada"));
+
+        if (emprestimo.getValor() > pessoa.getValorMaximoEmprestimo() ||
+            emprestimo.getParcelas() < pessoa.getValorMinimoParcela() ||
+            emprestimo.getNumeroParcelas() > 24) {
+            throw new IllegalArgumentException("Condições de empréstimo inválidas");
+        }
+
+        return emprestimoRepository.save(emprestimo);
     }
 
-    // Princípio SOLID: Single Responsibility
-    // Este método tem a única responsabilidade de deletar um empréstimo
+    // Método para executar o pagamento de um empréstimo
+    public void pagarEmprestimo(Long id) {
+        Emprestimo emprestimo = emprestimoRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Empréstimo não encontrado"));
+
+        emprestimo.setStatusPagamento("Pago");
+        emprestimoRepository.save(emprestimo);
+    }
+
+    // Método para deletar um empréstimo por ID
     public void delete(Long id) {
         emprestimoRepository.deleteById(id);
-    }
-
-    private EmprestimoDTO convertToDTO(Emprestimo emprestimo) {
-        // Lógica de conversão de Emprestimo para EmprestimoDTO
-        if (emprestimo == null) return null;
-        return new EmprestimoDTO(
-                emprestimo.getId(),
-                emprestimo.getValor(),
-                emprestimo.getNumeroParcelas(),
-                emprestimo.getStatusPagamento(),
-                emprestimo.getDataCriacao(),
-                emprestimo.getPessoa().getId()  // Supondo que Emprestimo tem um campo Pessoa
-        );
-    }
-
-    private Emprestimo convertToEntity(EmprestimoDTO emprestimoDTO) {
-        // Lógica de conversão de EmprestimoDTO para Emprestimo
-        if (emprestimoDTO == null) return null;
-        Emprestimo emprestimo = new Emprestimo(
-                emprestimoDTO.getId(),
-                emprestimoDTO.getValor(),
-                emprestimoDTO.getNumeroParcelas(),
-                emprestimoDTO.getStatusPagamento(),
-                emprestimoDTO.getDataCriacao()
-        );
-        emprestimo.setPessoa(new Pessoa(emprestimoDTO.getPessoaId()));  // Supondo que Emprestimo tem um campo Pessoa
-        return emprestimo;
     }
 }
--- GestaoEmprestimosEmpresaX/src/main/java/br/com/wefin/GestaoEmprestimosEmpresaX/service/PessoaService.java
+++ GestaoEmprestimosEmpresaX/src/main/java/br/com/wefin/GestaoEmprestimosEmpresaX/service/PessoaService.java
@@ -1,6 +1,5 @@
 package br.com.wefin.GestaoEmprestimosEmpresaX.service;
 
-import br.com.wefin.GestaoEmprestimosEmpresaX.dto.PessoaDTO;
 import br.com.wefin.GestaoEmprestimosEmpresaX.model.Pessoa;
 import br.com.wefin.GestaoEmprestimosEmpresaX.repository.PessoaRepository;
 import br.com.wefin.GestaoEmprestimosEmpresaX.factory.IdentificadorFactory;
@@ -9,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class PessoaService {
@@ -18,67 +16,34 @@ public class PessoaService {
     private PessoaRepository pessoaRepository;
 
     // Método para buscar todas as pessoas
-    public List<PessoaDTO> findAll() {
-        return pessoaRepository.findAll().stream()
-                .map(this::convertToDTO)
-                .collect(Collectors.toList());
+    public List<Pessoa> findAll() {
+        return pessoaRepository.findAll();
     }
 
     // Método para buscar uma pessoa por ID
-    public PessoaDTO findById(Long id) {
+    public Pessoa findById(Long id) {
         return pessoaRepository.findById(id)
-                .map(this::convertToDTO)
                 .orElseThrow(() -> new EntityNotFoundException("Pessoa não encontrada"));
     }
 
     // Método para salvar uma pessoa
-    // Responsável também por definir o TipoIdentificador e os valores de empréstimo
-    public PessoaDTO save(PessoaDTO pessoaDTO) {
+    public Pessoa save(Pessoa pessoa) {
         // Utiliza o padrão Strategy para validar o identificador
-        IdentificadorStrategy strategy = IdentificadorFactory.createIdentificadorStrategy(pessoaDTO.getIdentificador());
-        if (!strategy.validar(pessoaDTO.getIdentificador())) {
+        IdentificadorStrategy strategy = IdentificadorFactory.createIdentificadorStrategy(pessoa.getIdentificador());
+        if (!strategy.validar(pessoa.getIdentificador())) {
             throw new IllegalArgumentException("Identificador inválido");
         }
 
         // Define o TipoIdentificador e os valores de empréstimo com base na estratégia
-        pessoaDTO.setTipoIdentificador(strategy.getTipo());
-        pessoaDTO.setValorMinimoParcela(strategy.getValorMinimoParcela());
-        pessoaDTO.setValorMaximoEmprestimo(strategy.getValorMaximoEmprestimo());
+        pessoa.setTipoIdentificador(strategy.getTipo());
+        pessoa.setValorMinimoParcela(strategy.getValorMinimoParcela());
+        pessoa.setValorMaximoEmprestimo(strategy.getValorMaximoEmprestimo());
 
-        Pessoa pessoa = convertToEntity(pessoaDTO);
-        return convertToDTO(pessoaRepository.save(pessoa));
+        return pessoaRepository.save(pessoa);
     }
 
     // Método para deletar uma pessoa por ID
     public void delete(Long id) {
         pessoaRepository.deleteById(id);
-    }
-
-    // Conversão de Pessoa para PessoaDTO
-    private PessoaDTO convertToDTO(Pessoa pessoa) {
-        if (pessoa == null) return null;
-        return new PessoaDTO(
-                pessoa.getId(),
-                pessoa.getNome(),
-                pessoa.getIdentificador(),
-                pessoa.getDataNascimento(),
-                pessoa.getTipoIdentificador(),
-                pessoa.getValorMinimoParcela(),
-                pessoa.getValorMaximoEmprestimo()
-        );
-    }
-
-    // Conversão de PessoaDTO para Pessoa
-    private Pessoa convertToEntity(PessoaDTO pessoaDTO) {
-        if (pessoaDTO == null) return null;
-        return new Pessoa(
-                pessoaDTO.getId(),
-                pessoaDTO.getNome(),
-                pessoaDTO.getIdentificador(),
-                pessoaDTO.getDataNascimento(),
-                pessoaDTO.getTipoIdentificador(),
-                pessoaDTO.getValorMinimoParcela(),
-                pessoaDTO.getValorMaximoEmprestimo()
-        );
     }
 }
