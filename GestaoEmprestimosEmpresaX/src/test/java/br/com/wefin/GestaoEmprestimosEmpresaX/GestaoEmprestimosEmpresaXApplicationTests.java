9807ff73d849a07e16ae52d6599cb0f45f353f3f
Diogo Santos <diogo_2806@hotmail.com>
2023-09-21 14:47:16 -0300

    upload
    upload

---
+++ GestaoEmprestimosEmpresaX/src/test/java/br/com/wefin/GestaoEmprestimosEmpresaX/GestaoEmprestimosEmpresaXApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wefin.GestaoEmprestimosEmpresaX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoEmprestimosEmpresaXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
