bd2ecfc9a552ab056b942ddd6ef16382c7c12fb9
Diogo Santos <diogo_2806@hotmail.com>
2023-09-21 14:12:23 -0300

    Delete GestaoEmprestimosEmpresaX directory

--- GestaoEmprestimosEmpresaX/src/main/java/br/com/wefin/GestaoEmprestimosEmpresaX/validation/IdentificadorStrategy.java
+++
@@ -1,6 +0,0 @@
-package br.com.wefin.GestaoEmprestimosEmpresaX.validation;
-
-// Define uma estratégia para validar diferentes tipos de identificadores
-public interface IdentificadorStrategy {
-    boolean validar(String identificador);
-}
