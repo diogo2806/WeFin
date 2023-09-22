9e612fb3f8c8747aaf6284b0983733aa5c563675
diogo2806 <diogo_2806@hotmail.com>
2023-09-22 20:28:53 -0300

    update

--- GestaoEmprestimosEmpresaX/src/main/java/br/com/wefin/GestaoEmprestimosEmpresaX/controller/HomeController.java
+++ GestaoEmprestimosEmpresaX/src/main/java/br/com/wefin/GestaoEmprestimosEmpresaX/controller/HomeController.java
@@ -1,10 +1,12 @@
+package br.com.wefin.GestaoEmprestimosEmpresaX.controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     
-    @GetMapping({"/", ""})
+    @GetMapping({"/"})
     public String home() {
         return "redirect:/index.xhtml";
     }
