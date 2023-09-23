532e2d91d08491b2bcbb4912a313c036e4b062d7
diogo2806 <diogo_2806@hotmail.com>
2023-09-22 23:02:23 -0300

    update

--- GestaoEmprestimosEmpresaX/src/main/java/br/com/wefin/GestaoEmprestimosEmpresaX/controller/PessoaController.java
+++ GestaoEmprestimosEmpresaX/src/main/java/br/com/wefin/GestaoEmprestimosEmpresaX/controller/PessoaController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import br.com.wefin.GestaoEmprestimosEmpresaX.service.PessoaService;
 import br.com.wefin.GestaoEmprestimosEmpresaX.dto.PessoaDTO;  // Importe esta classe
 import java.util.List;  // Importe esta classe
@@ -19,4 +22,15 @@ public class PessoaController {
         model.addAttribute("pessoas", pessoas);
         return "listarPessoas";
     }
+
+    @PostMapping("/criarPessoa")
+    public String criarPessoa(@ModelAttribute PessoaDTO pessoaDTO) {
+        // Aqui você deve implementar a lógica para criar uma nova pessoa
+        // Você pode chamar o serviço para salvar a pessoa no banco de dados
+        pessoaService.save(pessoaDTO);
+
+        // Redirecione para a página de listagem de pessoas ou outra página apropriada
+        return "redirect:/listarPessoas";
+    }
+
 }
